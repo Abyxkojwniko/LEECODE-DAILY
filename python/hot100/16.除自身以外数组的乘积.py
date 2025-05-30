642429e61ff50019aab9a5af9aaad233f2b4ac4a
Abyxkojw <abyxkojw@163.com>
2025-05-30 19:05:04 +0800

    update 12,16

---
+++ python/hot100/16.除自身以外数组的乘积.py
@@ -0,0 +1,11 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        ans=[1]*n
+        for i in range(1,n):
+            ans[i]=nums[i-1]*ans[i-1]
+        right=nums[n-1]
+        for i in range(n-2,-1,-1):
+            ans[i]*=right
+            right*=nums[i]
+        return ans
