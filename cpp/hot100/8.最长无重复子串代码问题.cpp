9e365db9dd2681cbeadc6e895afae7a292e2e89a
Abyxkojw <abyxkojw@163.com>
2025-10-13 12:53:58 +0800

    fix mistakes and update 67"

--- cpp/hot100/8.最长无重复子串代码问题.cpp
+++ cpp/hot100/8.最长无重复子串代码问题.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
-        unordered_ser<char> map;
-        int left=0,right=0;
-        for(right;right<n;right++){
-            char ch=s[right];
+        int left = 0, right = 0;
+        int n = s.size();
+        int ans = 0;
+        unordered_set<char> map;
+        for(; right < n; ++right){
+            char ch = s[right];
             while(map.count(ch)){
-                char c=s[left++];
+                char c = s[left++];
                 map.erase(c);
             }
-            set.insert(ch);
-            ans=max(ans,right-left+1);
+            map.insert(ch);
+            ans = max(ans, right - left + 1);
         }
         return ans;
     }
--- cpp/hot100/9.找到字符串中所有字母异位词.cpp
+++ cpp/hot100/9.找到字符串中所有字母异位词.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
-        array<int,26> count_p{},count_s{};
-        for(auto c:p){
-            count_p[c-'a']++;
+        array<int, 26> count_p{}, count_s{};
+        for(auto c: p){
+            ++count_p[c - 'a'];
         }
-        for(int right=0;right<s.size();right++){
-            count_s[s[right]-'a']++;
-            int left=right-p.length()+1;
-            if(left<0) continue;
-            if(count_p==count_s) ans.push_back(left);
-            count_s[s[left]-'a']--;
+        for(int right = 0; right < s.size(); ++right){
+            ++count_s[s[right] - 'a'];
+            int left = right - p.size() + 1;
+            if(left < 0) continue;
+            if(count_p == count_s) ans.push_back(left);
+            --count_s[s[left] - 'a'];
         }
         return ans;
     }
